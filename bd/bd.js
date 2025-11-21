629e679af45e4cc2063ed844aec31fee16ede2ac
ElielUT <elielpa.ti24@utsjr.edu.mx>
2025-11-20 20:19:29 -0600

    QuitarSecret

--- bd/bd.js
+++ bd/bd.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose"
 
 async function conectarBD(){
-    //mongoose.connect("mongodb://localhost:27017/backend1") LOCAL
-    //mongoose.connect("mongodb+srv://root:root@cluster0.09dgql3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     try{
         const respuestaMongo = await mongoose.connect(process.env.SECRET_MONGO)
         console.log("Conexión con Mongo DB Atlas")
